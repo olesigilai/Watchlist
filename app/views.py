cfdb85b764b95b1dbf31b369db09695e24192c8e
olesigilai <ole_sigilai@yahoo.com>
2021-02-18 00:59:20 +0300

    images displayed and links worked

--- app/views.py
+++ app/views.py
@@ -1,21 +1,31 @@
 from flask import render_template
 from app import app
+from .request import get_movies,get_movie
 #Views
 @app.route('/')
-def index ():
+def index():
+
     '''
     View root page function that returns the index page and its data
     '''
+
+    # Getting popular movie
+    popular_movies = get_movies('popular')
+    upcoming_movie = get_movies('upcoming')
+    now_showing_movie = get_movies('now_playing')
     title = 'Home - Welcome to The best Movie Review Website Online'
-    message = 'uhuru kenyatta'
-    return render_template('index.html', message=message, title = title)
-from .request import get_movies
-@app.route('/movie/<int:movie_id>')
-def movie(movie_id):
+    return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
+
+@app.route('/movie/<int:id>')
+def movie(id):
+
     '''
     View movie page function that returns the movie details page and its data
     '''
-    return render_template('movie.html',id = movie_id)
+    movie = get_movie(id)
+    title = f'{movie.title}'
+
+    return render_template('movie.html',title = title,movie = movie)
 
 
 
