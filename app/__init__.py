106e40190e629488966cc46a9a14e5e31e1e9554
olesigilai <ole_sigilai@yahoo.com>
2021-02-18 23:14:09 +0300

    funcionality for search and reviews done

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from .config import DevConfig
 from flask_bootstrap import Bootstrap
 
+
 # Initializing application
 app = Flask(__name__,instance_relative_config = True)
 
@@ -11,3 +12,4 @@ app.config.from_pyfile('config.py')
 # Initializing Flask Extensions
 bootstrap = Bootstrap(app)
 from app import views
+from app import error
