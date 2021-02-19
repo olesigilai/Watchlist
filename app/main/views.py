ac5d9c1fd00b1d04fc367b314f5a9ee542897c86
olesigilai <ole_sigilai@yahoo.com>
2021-02-19 17:30:18 +0300

    migrate to the new structure

--- app/main/views.py
+++ app/main/views.py
@@ -1,12 +1,15 @@
-from flask import render_template
-from app import app
-from .request import get_movies,get_movie,search_movie
 from flask import render_template,request,redirect,url_for
-from .models import reviews
+from . import main
+from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-Review = reviews.Review
+from ..models import Review
+
+Review=Review
+
+
+
 # Views
-@app.route('/')
+@main.route('/')
 def index():
 
     '''
@@ -23,12 +26,12 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -39,21 +42,10 @@ def movie(id):
     reviews = Review.get_reviews(movie.id)
 
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
-def new_review(id):
-    form = ReviewForm()
-    movie = get_movie(id)
 
-    if form.validate_on_submit():
-        title = form.title.data
-        review = form.review.data
-        new_review = Review(movie.id,title,movie.poster,review)
-        new_review.save_review()
-        return redirect(url_for('movie',id = movie.id ))
 
-    title = f'{movie.title} review'
-    return render_template('new_review.html',title = title, review_form=form, movie=movie)
-@app.route('/search/<movie_name>')
+
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -63,3 +55,25 @@ def search(movie_name):
     searched_movies = search_movie(movie_name_format)
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
+
+
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+def new_review(id):
+
+    form = ReviewForm()
+
+    movie = get_movie(id)
+
+    if form.validate_on_submit():
+        title = form.title.data
+        review = form.review.data
+
+        new_review = Review(movie.id,title,movie.poster,review)
+        new_review.save_review()
+
+        return redirect(url_for('.movie',id = movie.id ))
+
+    title = f'{movie.title} review'
+    return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+
---
+++ start.sh
@@ -0,0 +1,4 @@
+export MOVIE_API_KEY='da722b948a6bfa00f72d44ea5772cfe4'
+export SECRET_KEY='kipkirui'
+
+python3.6 manage.py server
